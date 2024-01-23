d6bb354bec33d52277abce999ced4ce3379ee875
liusuxian <382185882@qq.com>
2024-01-23 11:47:00 +0800

    修改中间件和response

--- gf/gfmiddleware/middleware.go
+++ gf/gfmiddleware/middleware.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2024-01-19 21:15:17
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2024-01-22 21:35:59
+ * @LastEditTime: 2024-01-23 11:46:30
  * @Description:
  *
  * Copyright (c) 2024 by liusuxian email: 382185882@qq.com, All Rights Reserved.
@@ -13,14 +13,9 @@ import (
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/liusuxian/go-toolkit/gf/gflogger"
 	"net/http"
 )
-
-// CORS 允许接口跨域请求
-func CORS(req *ghttp.Request) {
-	req.Response.CORSDefault()
-	req.Middleware.Next()
-}
 
 // HandlerResponse 自定义返回中间件
 func HandlerResponse(req *ghttp.Request) {
@@ -32,6 +27,7 @@ func HandlerResponse(req *ghttp.Request) {
 	}
 
 	var (
+		// msg  string
 		err   = req.GetError()
 		res   = req.GetHandlerResponse()
 		rCode = gerror.Code(err)
@@ -40,9 +36,11 @@ func HandlerResponse(req *ghttp.Request) {
 		if rCode == gcode.CodeNil {
 			rCode = gcode.CodeInternalError
 		}
+		// msg = err.Error()
 	} else {
 		if req.Response.Status > 0 && req.Response.Status != http.StatusOK {
-			msg := http.StatusText(req.Response.Status)
+			// msg = http.StatusText(req.Response.Status)
+			errText := http.StatusText(req.Response.Status)
 			switch req.Response.Status {
 			case http.StatusNotFound:
 				rCode = gcode.CodeNotFound
@@ -52,11 +50,16 @@ func HandlerResponse(req *ghttp.Request) {
 				rCode = gcode.CodeUnknown
 			}
 			// It creates error as it can be retrieved by other middlewares.
-			err = gerror.NewCode(rCode, msg)
+			// err = gerror.NewCode(rCode, msg)
+			err = gerror.NewCode(rCode, errText)
 			req.SetError(err)
 		} else {
 			rCode = gcode.CodeOK
 		}
+	}
+
+	if err = req.GetError(); err != nil {
+		gflogger.Errorf(req.GetCtx(), "HandlerResponse Error: %+v", err)
 	}
 
 	req.Response.WriteJson(ghttp.DefaultHandlerResponse{
